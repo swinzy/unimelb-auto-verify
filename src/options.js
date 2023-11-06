ff250df7e9d64680d4ed71abc026ec80287555d0
Stephen Zhang <19246983+m1nicrusher@users.noreply.github.com>
2023-11-06 23:44:52 +1100

    Update preference interface to use bootstrap

--- src/options.js
+++ src/options.js
@@ -1,30 +1,41 @@
+const saveButton = document.getElementById("saveButton");
+const secretBox = document.getElementById("otpSecretInput");
+
+const NORMAL_BTN = "btn-primary";
+const SUCCESS_BTN = "btn-success";
+const SAVED = "Saved";
+const SAVE = "Save";
+const TIMEOUT_BTN_RES = 2000;
+
 // Saves options to chrome.storage
 const saveOptions = () => {
-    const secret = document.getElementById("secret").value;
-  
     chrome.storage.sync.set(
-      { secret: secret },
-      () => {
-        // Update status to let user know options were saved.
-        const status = document.getElementById("status");
-        status.textContent = "Options saved.";
-        setTimeout(() => {
-          status.textContent = "";
-        }, 750);
-      }
+        {secret: secretBox.value},
+        () => {
+            // Update status to let user know options were saved.
+            saveButton.classList.add(SUCCESS_BTN);
+            saveButton.classList.remove(NORMAL_BTN);
+            saveButton.innerText = SAVED;
+
+            // Restore status after a while
+            setTimeout(() => {
+                saveButton.classList.add(NORMAL_BTN);
+                saveButton.classList.remove(SUCCESS_BTN);
+                saveButton.innerText = SAVE;
+            }, TIMEOUT_BTN_RES);
+        }
     );
-  };
-  
-  // Restores select box and checkbox state using the preferences
-  // stored in chrome.storage.
-  const restoreOptions = () => {
+};
+
+// Restores options using the preferences stored in chrome.storage.
+const restoreOptions = () => {
     chrome.storage.sync.get(
-      { secret: "" },
-      (items) => {
-        document.getElementById("secret").value = items.secret;
-      }
+        {secret: ""},
+        (items) => {
+            secretBox.value = items.secret;
+        }
     );
-  };
-  
-  document.addEventListener('DOMContentLoaded', restoreOptions);
-  document.getElementById('save').addEventListener('click', saveOptions);
+};
+
+document.addEventListener("DOMContentLoaded", restoreOptions);
+saveButton.addEventListener("click", saveOptions);

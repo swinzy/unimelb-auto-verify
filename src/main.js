3c19b5f0c868a16d506b50570c9086ed4e139b17
m1nicrusher <19246983+m1nicrusher@users.noreply.github.com>
2023-11-07 01:56:18 +1100

    Update to adapt the new 3-step login interface
    I just don't get it why do they keep "downgrading" the login experience.

--- src/main.js
+++ src/main.js
@@ -1,49 +1,87 @@
- // Debugging indicator
-document.body.style.border = "5px solid red";
+// Debugging indicator
+document.body.style.border = "5px solid green";
 
+const checkOnMFAPage = () => {
+    // Prompt for Google Authenticator
+    var googleAuthElement = document.getElementsByClassName("challenge-authenticator--google_otp")
 
-// Load configurations and run extension
-chrome.storage.sync.get(
-  { secret: "" },
-  (items) => {
-    main(items.secret);
-  }
-);
-
-function main(OTP_SECRET) {
-  var oktaSignInElement = document.getElementById("okta-sign-in");
-
-  // Create an observer instance linked to the callback function
-  const observer = new MutationObserver(() => {
-    var identifierElement = document.querySelector('span[data-se="identifier"]');
-    var credentialInput = document.querySelector('input[name="credentials.passcode"]');
-    var verifyButton = document.querySelector('input[type="submit"]');
-    if (!identifierElement || !credentialInput) {
-      console.log("Not on Google Authenticator page, OTP is not calculated.");
-      return;
+    // Page does not contain Google Authenticator prompt
+    if (googleAuthElement.length === 0) {
+        console.log("UnimelbAutoVerify: Not on Google Authenticator page, extension standby.");
+        return false;
     }
+    return true;
+}
+
+const autoFillMFA = () => {
+
+    // Abort action if not on MFA page
+    if (!checkOnMFAPage()) return;
+
+    // Element contains username
+    var identifierElement = document.querySelector('span[data-se="identifier"]');
+
+    // Box to put MFA code
+    var credentialInput = document.querySelector('input[name="credentials.passcode"]');
+
+    // Button to submit MFA code
+    var verifyButton = document.querySelector('input[type="submit"]');
+
+    // Abort action if:
+    // Page does not contain username, does not contain MFA code box, or otpSecret is not provided
+    if (!identifierElement || !credentialInput || !otpSecret || otpSecret === "") {
+        console.log("UnimelbAutoVerify: Page does not contain information required, action not applied.");
+        return;
+    }
+
     var username = identifierElement.innerText;
     var totp = new jsOTP.totp();
-    var timeCode = totp.getOtp(OTP_SECRET);
+    var timeCode = totp.getOtp(otpSecret);
+
     console.log(`User name: ${username}, OTP: ${timeCode}`);
+
+    // Put MFA code in
     credentialInput.value = timeCode;
+
+    // Send event letting web page know code has been entered
     let event = new Event("input", {
-      "bubbles": true,
-      "cancelable": true
+        "bubbles": true,
+        "cancelable": true
     });
     credentialInput.dispatchEvent(event);
+
+    // Automatically click "Verify" if possible
     if (verifyButton)
-      verifyButton.click();
+        verifyButton.click();
     else
-      console.log("NO BUTTON");
-    observer.disconnect()
-    return;
-  });
+        console.log("UnimelbAutoVerify: Verify button not found. Please manually submit the code.");
 
+    // Job done, stop autofill.
+    observer.disconnect();
+};
 
-  // Start observing the document with the configured parameters
-  observer.observe(oktaSignInElement, {
-    childList: true,  // Observe child elements being added/removed
-    subtree: true     // Observe all descendants
-  });
+const startAutofill = () => {
+    var oktaSignInElement = document.getElementById("okta-sign-in");
+
+    // Start observing DOM change
+    observer.observe(oktaSignInElement, {
+        childList: true,  // Observe child elements being added/removed
+        subtree: true     // Observe all descendants
+    });
 }
+
+// Every time DOM changes, check if it can autofill MFA
+const observer = new MutationObserver(autoFillMFA);
+
+// Do not expose otp secret to window
+let otpSecret = "";
+
+// Load configurations and start autofill
+chrome.storage.sync.get(
+    {secret: ""},
+    (items) => {
+        // Only start extension when relevant information is ready
+        startAutofill();
+        otpSecret = items.secret;
+    }
+);
